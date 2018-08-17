63beb2d468f8b25d4ed841d4aae97c71cb446b69
dengxiang <790309793@qq.com>
2018-08-17 09:10:46 +0800

    element-UI

---
+++ src/components/container.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="wrap">
+      <div class="left">
+        <left-menu/>
+      </div>
+      <div class="right">
+        <keep-alive>
+          <router-view></router-view>
+        </keep-alive>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import '../static/css/reset.css'
+import leftMenu from './leftMenu'
+export default {
+  components: {
+    leftMenu
+  }
+}
+</script>
+<style lang="less" scoped>
+#app{
+  height: 100%;
+  .wrap{
+    min-height: 100%;
+    display: flex;
+    .left{
+      max-width: 200px;
+    }
+    .right{
+      flex: 1;
+    }
+  }
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,22 @@
+<!--  -->
+<template>
+  <div>{{this.$route.params.number}}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
